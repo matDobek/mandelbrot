e110ef2b31f8071e2b8c993dd4e2b2718b211a1f
Mateusz Dobek <mat.dobek@gmail.com>
2018-05-11 00:05:14 +0200

    Generate GIF, instead of static images

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 extern crate crossbeam;
-extern crate image;
 extern crate num;
+extern crate gif;
 
-use num::Complex;
+use std::io::{self, Write};
 use std::str::FromStr;
-use image::ColorType;
-use image::png::PNGEncoder;
 use std::fs::File;
-use std::io::Write;
+use std::borrow::Cow;
+use num::Complex;
+use gif::SetParameter;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -16,23 +16,115 @@ fn main() {
         writeln!(
             std::io::stderr(),
             r#"
-            Usage: {0} FILE PIXELS UPPERLEFT LOWERRIGHT
-            Example: {0} mandel.png 1000x750 -1.20,0.35 -1.0,0.20
-        "#,
+                Usage: {0} NAME NO_OF_FRAMES ZOOM_SPEED POINT
+                Example: {0} mandel 50 0.1 -0.77568377,0.13646737
+            "#,
             args[0]
         ).unwrap();
 
         std::process::exit(1);
     }
 
-    let size = parse_pair(&args[2], 'x').expect("error while parsing pixel size");
-    let upper_left = parse_complex(&args[3]).expect("error while parsing upper left point");
-    let lower_right = parse_complex(&args[4]).expect("error while parsing upper left point");
+    let size = (750, 750);
+    let name = &args[1];
+    let number_of_frames = usize::from_str(&args[2]).unwrap();
+    let zoom_speed = f64::from_str(&args[3]).unwrap();
+    let central_point = parse_complex(&args[4]).expect("error while parsing upper left point");
+    let (upper_left, lower_right) = fetch_upper_left_and_lower_right_coordinates_based_on_central_point(central_point);
 
+    generate_gif(name, number_of_frames, zoom_speed, size, upper_left, lower_right);
+}
+
+/// Fetch touple with `(upper_left, lower_right)` points, based on the `central_point`.
+fn fetch_upper_left_and_lower_right_coordinates_based_on_central_point(central_point: Complex<f64>) -> (Complex<f64>, Complex<f64>){
+    let upper_left: Complex<f64> = Complex { re: -2.0, im: 2.0 };
+    let lower_right: Complex<f64> = Complex {
+        re: central_point.re + ((upper_left.re - central_point.re).abs()).abs(),
+        im: central_point.im - ((upper_left.im - central_point.im).abs()).abs(),
+    };
+
+    (upper_left, lower_right)
+}
+
+fn generate_gif(
+    filename: &str,
+    number_of_frames: usize,
+    zoom_speed: f64,
+    size: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+    ) -> () {
+
+    let mut frame_states = Vec::new();
+
+    let mut zoomed_upper_left = upper_left;
+    let mut zoomed_lower_right = lower_right;
+
+    for index in 0..number_of_frames {
+        let frame_state = generate_frame(size, zoomed_upper_left, zoomed_lower_right);
+        frame_states.push(frame_state);
+
+        let width = (zoomed_upper_left.re - zoomed_lower_right.re).abs();
+        let height = (zoomed_upper_left.im - zoomed_lower_right.im).abs();
+
+        let zoomed_width = width * zoom_speed;
+        let zoomed_height = height * zoom_speed;
+
+        zoomed_upper_left = zoomed_upper_left + Complex {
+            re: zoomed_width,
+            im: -zoomed_height,
+        };
+        zoomed_lower_right = zoomed_lower_right + Complex {
+            re: -zoomed_width,
+            im: zoomed_height,
+        };
+
+        print!("\rProgress: {}%", ((1.0 + index as f32) / number_of_frames as f32 * 100.0).round());
+        io::stdout().flush().unwrap();
+    }
+
+    let filename_with_extension = format!("{}.gif", filename);
+    let mut image = File::create(filename_with_extension).unwrap();
+    let mut encoder = gif::Encoder::new(&mut image, size.0 as u16, size.1 as u16, &fetch_color_map()).unwrap();
+    encoder.set(gif::Repeat::Infinite).unwrap();
+
+    for i in 0..frame_states.len() * 2 {
+        let index = match i {
+            i if i < frame_states.len()  => i,
+            _ => 2*frame_states.len() - 1 - i,
+        };
+
+        let mut frame = gif::Frame::default();
+        frame.width = size.0 as u16;
+        frame.height = size.1 as u16;
+        frame.buffer = Cow::Borrowed(&*frame_states[index]);
+        encoder.write_frame(&frame).unwrap();
+    }
+}
+
+fn fetch_color_map() -> [u8; 256 * 3] {
+    let mut color_map: [u8; 256*3] = [0; 256*3];
+
+    for i in 0..256 {
+        let rgb = [255 - i, 255 - i, 255 - i];
+
+        for j in 0..rgb.len() {
+            color_map[i * rgb.len() + j] = rgb[j] as u8;
+        }
+    }
+
+    color_map
+}
+
+/// Generate single frame, represented as `Vec<u8>` where `u8` values represent how many iterations there were needed for given point, to leave the set.
+fn generate_frame(
+    size: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+    ) -> Vec<u8> {
     let mut pixels = vec![0; size.0 * size.1];
 
     let threads = 4;
-    println!("Threads: {}", threads);
 
     let rows_per_band = size.1 / threads + 1;
 
@@ -55,16 +147,7 @@ fn main() {
         });
     }
 
-    write_image(&args[1], &pixels, size).expect("error writing file");
-}
-
-fn write_image(filename: &str, pixels: &[u8], size: (usize, usize)) -> Result<(), std::io::Error> {
-    let output = File::create(filename)?;
-
-    let encoder = PNGEncoder::new(output);
-    encoder.encode(&pixels, size.0 as u32, size.1 as u32, ColorType::Gray(8))?;
-
-    Ok(())
+    return pixels;
 }
 
 /// Render a rectangle of the Mandelbrot set into a buffer of pixels
@@ -86,8 +169,8 @@ fn render(
         for column in 0..size.0 {
             let point = pixel_to_point(size, (column, row), upper_left, lower_right);
             pixels[column + row * size.0] = match escape_time(point, 255) {
-                None => 0,
-                Some(count) => 255 - count as u8,
+                None => 255,
+                Some(count) => count as u8,
             };
         }
     }
